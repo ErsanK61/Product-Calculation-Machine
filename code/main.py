2a52d9b90c7a502358705f9c18f4a9bc86a08733
Ersan Kahraman <115080173+ErsanK61@users.noreply.github.com>
2023-02-14 14:10:03 +0100

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,6 @@
+import numpy 
+import Theano #KI kombinierbar mit numpy
+import Tensorflow #(search Algortihmen etc)
+import pandas 
+import SciKit-Learn #data science daten hinzufügen etc
+import Keras #neuronales netzwerk - prediction etc
